b161d1f94cf1b3128c8ca9ad26351c44aad7aa91
Haniel Kamau <ngangakehlani28@gmail.com>
2023-07-11 16:19:07 +0300

    third commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Welcome Home Haniel, Let's get started!!";
 });
+
+
